075c5866d125cc1d9398f05f36500250d69917dd
svlsml <svsmailova@gmail.com>
2021-04-20 20:24:32 +0300

    refactor code

--- Mase/maze.py
+++ Mase/maze.py
@@ -1,78 +1,83 @@
-# Implements the Maze ADT using a 2-D array.
+"""Implemention of the Maze ADT using a 2-D array."""
 from arrays import Array2D
 from lliststack import Stack
 
-class Maze :
-    # Define constants to represent contents of the maze cells.
-    MAZE_WALL = " *"
+
+class Maze:
+    """Define constants to represent contents of the maze cells."""
+    MAZE_WALL = "*"
     PATH_TOKEN = "x"
     TRIED_TOKEN = "o"
 
-    # Creates a maze object with all cells marked as open.
-    def __init__( self, num_rows, num_cols ):
-        self._mazeCells = Array2D( num_rows, num_cols )
-        self._startCell = None
-        self._exitCell = None
+    def __init__(self, num_rows, num_cols):
+        """Creates a maze object with all cells marked as open."""
+        self._maze_cells = Array2D(num_rows, num_cols)
+        self._start_cell = None
+        self._exit_cell = None
 
-    # Returns the number of rows in the maze.
-    def num_rows( self ):
-        return self._mazeCells.num_rows()
+    def num_rows(self):
+        """Returns the number of rows in the maze."""
+        return self._maze_cells.num_rows()
 
-    # Returns the number of columns in the maze.
-    def num_cols( self ):
-        return self._mazeCells.num_cols()
+    def num_cols(self):
+        """Returns the number of columns in the maze."""
+        return self._maze_cells.num_cols()
 
-    # Fills the indicated cell with a "wall" marker.
-    def setWall( self, row, col ):
+    def set_wall(self, row, col):
+        """Fills the indicated cell with a "wall" marker."""
         assert row >= 0 and row < self.num_rows() and \
                col >= 0 and col < self.num_cols(), "Cell index out of range."
-        self._mazeCells[row, col] = self.MAZE_WALL
+        self._maze_cells[row, col] = self.MAZE_WALL
 
-    # Sets the starting cell position.
-    def setStart( self, row, col ):
+    def set_start(self, row, col):
+        """Sets the starting cell position."""
         assert row >= 0 and row < self.num_rows() and \
                col >= 0 and col < self.num_cols(), "Cell index out of range."
-        self._startCell = _CellPosition( row, col )
+        self._start_cell = _CellPosition(row, col)
 
-    # Sets the exit cell position.
-    def setExit( self, row, col ):
+    def set_exit(self, row, col):
+        """Sets the exit cell position."""
         assert row >= 0 and row < self.num_rows() and \
                col >= 0 and col < self.num_cols(), "Cell index out of range."
-        self._exitCell = _CellPosition( row, col )
+        self._exit_cell = _CellPosition(row, col)
 
-    # Attempts to solve the maze by finding a path from the starting cell
-    # to the exit. Returns True if a path is found and False otherwise.
-    def findPath( self ):
+
+    def find_path(self):
+        """
+        Attempts to solve the maze by finding a path from the starting cell
+        to the exit. Returns True if a path is found and False otherwise.
+        """
         pass
 
-    # Resets the maze by removing all "path" and "tried" tokens.
-    def reset( self ):
+    def reset(self):
+        """Resets the maze by removing all "path" and "tried" tokens."""
         pass
 
-    # Prints a text-based representation of the maze.
-    def draw( self ):
+    def __str__(self):
+        """Returns a text-based representation of the maze."""
         pass
 
-    # Returns True if the given cell position is a valid move.
-    def _validMove( self, row, col ):
+    def _valid_move(self, row, col):
+        """Returns True if the given cell position is a valid move."""
         return row >= 0 and row < self.num_rows() \
                and col >= 0 and col < self.num_cols() \
-               and self._mazeCells[row, col] is None
+               and self._maze_cells[row, col] is None
 
-    # Helper method to determine if the exit was found.
-    def _exitFound( self, row, col ):
-        return row == self._exitCell.row and col == self._exitCell.col
+    def _exit_found(self, row, col):
+        """Helper method to determine if the exit was found."""
+        return row == self._exit_cell.row and col == self._exit_cell.col
 
-    # Drops a "tried" token at the given cell.
-    def _markTried( self, row, col ):
-        self._mazeCells[row, col] = self.TRIED_TOKEN
+    def _mark_tried(self, row, col):
+        """Drops a "tried" token at the given cell."""
+        self._maze_cells[row, col] = self.TRIED_TOKEN
 
-    # Drops a "path" token at the given cell.
-    def _markPath( self, row, col ):
-        self._mazeCells[row, col] = self.PATH_TOKEN
+    def _mark_path(self, row, col):
+        """Drops a "path" token at the given cell."""
+        self._maze_cells[row, col] = self.PATH_TOKEN
 
-# Private storage class for holding a cell position.
-class _CellPosition( object ):
-    def __init__( self, row, col ):
+
+class _CellPosition(object):
+    """Private storage class for holding a cell position."""
+    def __init__(self, row, col):
         self.row = row
         self.col = col
--- Mase/solvemaze.py
+++ Mase/solvemaze.py
@@ -1,47 +1,50 @@
 # Program for building and solving a maze.
 from maze import Maze
 
+
 # The main routine.
 def main():
-    maze = buildMaze( "mazefile.txt" )
-    if maze.findPath() :
-        print( "Path found...." )
-        maze.draw()
-    else :
-        print( "Path not found...." )
-        maze.draw()
+    maze = build_maze("mazefile5.txt")
+    if maze.find_path():
+        print("Path found....")
+    else:
+        print("Path not found....")
+    print(maze)
 
-# Builds a maze based on a text format in the given file.
-def buildMaze( filename ):
-    infile = open( filename, "r" )
+
+def build_maze(filename):
+    """Builds a maze based on a text format in the given file."""
+    infile = open(filename, "r")
 
     # Read the size of the maze.
-    nrows, ncols = readValuePair( infile )
-    maze = Maze( nrows, ncols )
+    nrows, ncols = read_value_pair(infile)
+    maze = Maze(nrows, ncols)
 
     # Read the starting and exit positions.
-    row, col = readValuePair( infile )
-    maze.setStart( row, col )
-    row, col = readValuePair( infile )
-    maze.setExit( row, col )
+    row, col = read_value_pair(infile)
+    maze.set_start(row, col)
+    row, col = read_value_pair(infile)
+    maze.set_exit(row, col)
 
     # Read the maze itself.
-    for row in range( nrows ) :
+    for row in range(nrows):
         line = infile.readline()
-        for col in range( len(line) ) :
-            if line[col] == "*" :
-                maze.setWall( row, col )
+        for col in range(len(line)):
+            if line[col] == "*":
+                maze.set_wall(row, col)
 
     # Close the maze file and return the newly constructed maze.
     infile.close()
 
     return maze
 
-# Extracts an integer value pair from the given input file.
-def readValuePair( infile ):
+
+def read_value_pair(infile):
+    """Extracts an integer value pair from the given input file."""
     line = infile.readline()
     valA, valB = line.split()
     return int(valA), int(valB)
+
 
 # Call the main routine to execute the program.
 main()
